3cf6f8e9ef0fa5f42aba3c7b9ed11332425989a4
dlbbld <daniel.baechli@gmx.ch>
2020-07-15 14:35:31 +0200

    reverted the level to v1.0

--- src/game.js
+++ src/game.js
@@ -184,225 +184,292 @@ window.onload = function(){
 			'',
 			'kkkkkkkk'
 		);
-		
-		//test walking
+
+		// first pawn
+		block(
+			{showThreat:'p'},
+			'',
+			'',
+			'    p',
+			'',
+			'',
+			'',
+			'',
+			''
+		);
+
+		// scattered pawns
+		block(
+			'  pppp',
+			'  pppp',
+			'',
+			'  p',
+			'',
+			'     p',
+			'',
+			' p'
+		);
+
+		// pawn rows
+		block(
+			'',
+			'pppppp',
+			'',
+			'  pppppp',
+			'',
+			'pppppp',
+			'',
+			''
+		);
+
+		// triangle
+		block(
+			'',
+			'pp   ppp',
+			'  p p',
+			'   p',
+			'',
+			'',
+			'',
+			''
+		);
+
+		// sawtooth
+		block(
+			'',
+			'   pp',
+			'p p  p p',
+			' p    p',
+			'',
+			'',
+			'',
+			''
+		);
+
+		// wedges
+		block(
+			'',
+			'    ppp',
+			'     p',
+			'ppp',
+			' p   ppp',
+			'      p',
+			'',
+			''
+		);
+
+		//CHECK POINT 1
+		checkPoint();
+
+		// first rook
+		block(
+			{showThreat:'r'},
+			'',
+			'   p',
+			'',
+			'   r',
+			'',
+			'   p',
+			'',
+			''
+		);
+
+		// rook diag
 		block(
 			'',
 			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
-			'',
+			'r',
+			' r',
+			'  r',
+			'   r',
+			'    r',
+			'     r',
+			'pppppp'
+		);
+
+		// rook rows
+        block(
+            '',
+            '',
+            '',
+            '       p',
+            '    rp r',
+            '',
+            '',
+            'pp',
+            'r   p  p',
+            'p'
+        );
+
+		// rook labyrinth
+		block(
+            '',
+            'p     r',
+            'r     p',
+            'r    p',
+            '',
+            '   p',
+            '   p',
+            'p  ppppp',
+            ''
+        );
+
+		//CHECK POINT 2
+        checkPoint();
+
+		//10 first bishop
+		block(
+			{showThreat:'b'},
+			'',
+			'',
+			'',
+			'',
+			'...b',
+			'',
+			'',
+			'',
+			''
+		);
+
+		//11 bishop field
+		block(
+			'',
+			' p p p',
+			'b b b b',
 			'',
 			'',
 			'',
 			'',
 			'',
 			''
-		);			
+		);
 
-		//BEGIN new level
-		
-		//knights level one dungeon
+		//12 rooks & bishops simple
 		block(
-				'    p   ',
-				'     b  ',
-				'',
-				'',
-				'',
-				'      k ',
-				'  k     ',
-				' k     k',
-				'k  k   k',
-				'k       ',
-				'      k ',
-				'',
-				'',
-				'',
-				'',
-				'',
-				'',
-				'',
-				''
-			);
+			'',
+			'r.p..p.r',
+			'p......p',
+			'',
+			'b      b',
+			'pp....pp',
+			'',
+			''
+		);
 
-		checkPoint();
-		
 
-		// rook and bishop dungeon
+		//13 pawns, bishops and rooks
 		block(
-				'',
-				'',
-				'',
-				'',
-				'',
-				'',
-				'p p p   ',
-				' p p    ',
-				'rbrbrbrb',
-				'brbrbrbr',
-				'rbrbrbrb',
-				'brbrbrbr',
-				'rbrrrbrb',
-				'brbrbrbr',
-				' p b    ',
-				'',
-				'',
-				''
-			);
+			'',
+			'r',
+			' r  p..b',
+			'     ..r',
+			'....p',
+			'...p',
+			'..p',
+			'pp.....p'
+		);
 
+		//CHECK POINT 3
 		checkPoint();
 
-		
-		//knights level two dungeon
+		//14 first knight
 		block(
-				'',
-				'',
-				'',
-				'',
-				'',
-				'',
-				'',
-				'   kk   ',
-				'     kk ',
-				'',
-				'     k  ',
-				'k kkk   ',
-				'',
-				'',
-				'kkk k   ',
-				'',
-				'',
-				'',
-				'',
-				'',
-				''
-			);
+			{showThreat:'k'},
+			'',
+			'',
+			'   k',
+			'',
+			'',
+			'',
+			'',
+			''
+		);
 
-		checkPoint();
-			
-			
-		// rook and bishop and knights dungeon
+		//15 knight rows
 		block(
-				'',
-				'',
-				'',
-				'',
-				'brbrbrrb',
-				'rbrbrbrb',
-				'bbrrbkbr',
-				'brrbkbrb',
-				'kbbrbkrk',
-				'krrbrbrb',
-				'kkbrbrbk',
-				'kbrbrbrr',
-				'  b   b ',
-				'       b',
-				'',
-				'',
-				''
-			);
-		
-		//no checkpoint needed for last level
-		//END new level
+			'p',
+			'r.....p',
+			'',
+			'',
+			'......kk',
+			'',
+			'pkk',
+			''
+		);
+
+		//rooks bishop and knight
+        block(
+            'p   pppp',
+            'r    b r',
+            '   p b p',
+            '',
+            '',
+            '',
+            ' k',
+            '',
+            '',
+            ''
+        );
+
+        //CHECK POINT 4
+        checkPoint();
+
+        //first land mine
+        block(
+            {showThreat:'l'},
+            '',
+            '',
+            '',
+            '   l',
+            '',
+            '',
+            '',
+            ''
+        );
+
+        //land mines
+        block(
+            'l.llllll',
+            'l.l....l',
+            '..l.llll',
+            '.ll.l   ',
+            'l...l l ',
+            'l.lll l ',
+            'l.    l ',
+            'lllllll '
+        );
+
+		//land mines and pawns
+        block(
+            ' p  pll',
+            ' l p   l',
+            'lpl p p ',
+            '     lp',
+            'll ll l',
+            ' l  l',
+            '',
+            ''
+        );
+
+        //k,r,b,p,l
+        block(
+            '',
+            '  r',
+            '  llk',
+            '     .',
+            '   l ..',
+            '   p  pb',
+            '  . .  l',
+            '  p  l',
+            '',
+            '',
+            '',
+            '',
+            ''
+        );
+
+        //CHECK POINT 5
+        checkPoint();
 
         block(
             '',
